22be566375bf9004f41c32bc30b4d2435d8619ac
llithium <barrowjr0@gmail.com>
2024-03-02 08:29:45 -0500

    Add flags in rates view

--- src/CurrencyRates.jsx
+++ src/CurrencyRates.jsx
@@ -68,10 +68,10 @@ export default function CurrencyRates(props) {
           {props.viewExchangeRatesOptions[0].map((option, index) => {
             return (
               <li className="ratesList" key={index}>
-                <span className="exchangeRate">
-                  {props.viewExchangeRates[0][index]}
-                </span>
-                {option}
+                {props.viewExchangeRates[0][index]} {option}
+                <span
+                  className={`exchangeRate fi ${currencyToFlag(option)}`}
+                ></span>
                 {/* TODO: Fix flags in rates viewer */}
                 {/* <span className={`fi ${flags[index]}`}></span> */}
               </li>
@@ -81,4 +81,105 @@ export default function CurrencyRates(props) {
       </div>
     </div>
   );
+}
+
+function currencyToFlag(country) {
+  switch (country) {
+    case "AUD":
+      return "fi-au";
+      break;
+    case "BGN":
+      return "fi-bg";
+      break;
+    case "BRL":
+      return "fi-br";
+      break;
+    case "CAD":
+      return "fi-ca";
+      break;
+    case "CHF":
+      return "fi-ch";
+      break;
+    case "CNY":
+      return "fi-cn";
+      break;
+    case "CZK":
+      return "fi-cz";
+      break;
+    case "DKK":
+      return "fi-dk";
+      break;
+    case "GBP":
+      return "fi-gb";
+      break;
+    case "EUR":
+      return "fi-eu";
+      break;
+    case "HKD":
+      return "fi-hk";
+      break;
+    case "HUF":
+      return "fi-hu";
+      break;
+    case "IDR":
+      return "fi-id";
+      break;
+    case "ILS":
+      return "fi-il";
+      break;
+    case "INR":
+      return "fi-in";
+      break;
+    case "ISK":
+      return "fi-is";
+      break;
+    case "JPY":
+      return "fi-jp";
+      break;
+    case "KRW":
+      return "fi-kr";
+      break;
+    case "MXN":
+      return "fi-mx";
+      break;
+    case "MYR":
+      return "fi-my";
+      break;
+    case "NOK":
+      return "fi-no";
+      break;
+    case "NZD":
+      return "fi-nz";
+      break;
+    case "PHP":
+      return "fi-ph";
+      break;
+    case "PLN":
+      return "fi-pl";
+      break;
+    case "RON":
+      return "fi-ro";
+      break;
+    case "SEK":
+      return "fi-se";
+      break;
+    case "SGD":
+      return "fi-sg";
+      break;
+    case "THB":
+      return "fi-th";
+      break;
+    case "TRY":
+      return "fi-tr";
+      break;
+    case "USD":
+      return "fi-us";
+      break;
+    case "ZAR":
+      return "fi-za";
+      break;
+
+    default:
+      break;
+  }
 }
